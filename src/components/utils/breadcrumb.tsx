b3387ff9cac0b953b1764ed632eade260a57634d
axantillon <aantillon.leiva@gmail.com>
2023-04-04 13:25:50 -0600

    add linking back to home

--- src/components/utils/breadcrumb.tsx
+++ src/components/utils/breadcrumb.tsx
@@ -2,12 +2,13 @@
 
 import { FC } from "react";
 import { usePathname } from 'next/navigation';
+import Link from "next/link";
 
 
 export const BreadCrumb: FC = () => {
     const pathname = usePathname();
 
     return (
-        <span className="select-none"> andres antillon{pathname == '/' ? '' : pathname} </span>
+        <span className="select-none"> <Link href={'/'} className="px-2 rounded-sm hover:bg-zinc-200">andres antillon</Link>{pathname == '/' ? '' : pathname} </span>
     )
 }
