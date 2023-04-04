8e7cb167a97015611ae79851eea3cfb419f6e0d8
axantillon <aantillon.leiva@gmail.com>
2023-04-03 19:38:52 -0600

    add fix for dep bug

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack: (config) => { // necessary to avoid weird walletconnect dependency errors
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 }
 
 module.exports = nextConfig
